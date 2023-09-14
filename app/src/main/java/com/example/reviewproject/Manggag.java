c96ca3418113370692e01a964c6369882f67e3af
gamjaseob <130289088+gamjaseob@users.noreply.github.com>
2023-09-14 11:36:25 +0900

    망각 함수 수정

--- app/src/main/java/com/example/reviewproject/Manggag.java
+++ app/src/main/java/com/example/reviewproject/Manggag.java
@@ -75,29 +75,32 @@ public class Manggag extends AppCompatActivity {
         // + if ( 망각진행률 == 60% ) 이면 AddReviewList ( 리뷰리스트에 추가 ) 메서드 실행
         AddReviewList(Subject);
 
-        Drawgraph();
+        //Drawgraph();
     }
 
     //망각곡선 그리는 함수
     private void Drawgraph(Calendar cal) {
         Calendar c = Calendar.getInstance();
 
+        //현재시간 더블형 변환
         double minute1 = c.get(Calendar.MINUTE) + 1;
         double hour1 = c.get(Calendar.HOUR);
         double day1 = c.get(Calendar.DAY_OF_MONTH);
         double month1 = c.get(Calendar.MONTH) + 1;
 
+        //공부종료시간 더블형 변환
         double minute2 = cal.get(Calendar.MINUTE);
         double hour2 = cal.get(Calendar.HOUR);
         double day2 = cal.get(Calendar.DAY_OF_MONTH);
         double month2 = cal.get(Calendar.MONTH) + 1;
 
-
+        //현재시간 - 공부 종료 시간
         double minute = minute1 - minute2;
         double hour = hour1 - hour2;
         double day = day1 - day2;
         double month = month1 - month2;
 
+        //시간 더하기
         double nowtime = minute + (hour * 60) + (day * 1440) + (month * 44640);
 
         //double  = 184 / (Math.pow(Math.log(120.0), 1.25) + 1.84);
@@ -157,7 +160,7 @@ public class Manggag extends AppCompatActivity {
             Calendar cal = Calendar.getInstance();
             cal.setTime(date);
 
-            cal.add(Calendar.HOUR, -3);
+            //cal.add(Calendar.HOUR, -3);
 
             Drawgraph(cal);
         });
@@ -178,7 +181,8 @@ public class Manggag extends AppCompatActivity {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 subjectDocId = document.getId();
 
-                                //StudyTimeLoad(fileName, subjectDocId);
+                                //종료시간 불러오는 함수
+                                StudyTimeLoad(fileName, subjectDocId);
 
                                 // 이 부분에 망각 곡선 작성 :onCreate() 함수부터 Test 바람
 
